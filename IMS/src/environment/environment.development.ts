ef0763fedb3f7017a127714d3cc82fa75caa8723
shibaramsathua09 <Shibaram.Sathua@cognizant.com>
2025-07-05 20:08:00 +0530

    Update environment.development.ts

--- IMS/src/environment/environment.development.ts
+++ IMS/src/environment/environment.development.ts
@@ -1,3 +1,5 @@
 export const environment ={
     apiBaseUrl : 'http://localhost:5123/api'
+  //this creates a constant object named environment and exports it so that it can be used in other part.
+  //This defines the base URL for your backend API.
 }
