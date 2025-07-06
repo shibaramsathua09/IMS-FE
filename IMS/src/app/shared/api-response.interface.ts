3e92b8c724353ab5e6390841e3e7b0a6905c7b44
shibaramsathua09 <Shibaram.Sathua@cognizant.com>
2025-07-06 16:39:23 +0530

    Update api-response.interface.ts

--- IMS/src/app/shared/api-response.interface.ts
+++ IMS/src/app/shared/api-response.interface.ts
@@ -1,5 +1,8 @@
 export interface ApiResponse<T = any>{
-    isSuccess : boolean;
-    message: string;
-    data: T;
+    isSuccess : boolean;//indicated if the api call was success
+    message: string;//a message from the server
+    data: T;//the actual data returned from the api
 }
+/*Ensures consistent structure for all API responses.
+Makes it easier to handle success and error cases.
+Improves type safety in your Angular app.*/
