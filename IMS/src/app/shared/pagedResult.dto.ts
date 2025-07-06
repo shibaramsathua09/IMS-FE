38f8a79e1d73922418d3ba39deabfbb828ec4da1
shibaramsathua09 <Shibaram.Sathua@cognizant.com>
2025-07-06 16:35:55 +0530

    Update pagedResult.dto.ts

--- IMS/src/app/shared/pagedResult.dto.ts
+++ IMS/src/app/shared/pagedResult.dto.ts
@@ -1,12 +1,21 @@
 export interface PagedResult<T> {
 
-    items: T[];
+    items: T[];//the actual data items on the current page
   
-    pageNumber: number;
+    pageNumber: number;//the current page number
   
-    pageSize: number;
+    pageSize: number;//how many items are shown per page
   
-    totalCount: number;
+    totalCount: number;//Total number of items across all  pages
   
   }
-   
+   /*This is a generic interface used to represent paginated data 
+   — data that is split across multiple pages (like search results, product listings, etc.).
+   This interface helps you:
+
+Standardize how paginated data is handled across your app.
+Easily manage pagination logic in your frontend.
+Communicate clearly with APIs that return paginated results.
+
+When you call an API that returns paginated data, you use PagedResult<T> to type the response.
+   */
