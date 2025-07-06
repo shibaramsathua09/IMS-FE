942075c72bd747ac9e6abcf7438b9517c32527b0
shibaramsathua09 <Shibaram.Sathua@cognizant.com>
2025-07-06 18:15:01 +0530

    Update confirm-dialog.component.ts

--- IMS/src/app/shared/confirm-dialog/confirm-dialog.component.ts
+++ IMS/src/app/shared/confirm-dialog/confirm-dialog.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { MatDialogRef } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-confirm-dialog',
-  imports: [],
+  imports: [MatDialogModule,CommonModule,MatButtonModule],
   templateUrl: './confirm-dialog.component.html',
   styleUrl: './confirm-dialog.component.css'
 })
 export class ConfirmDialogComponent {
-
+  constructor(public dialogRef: MatDialogRef<ConfirmDialogComponent>) {}
+  confirm(): void {
+    this.dialogRef.close(true);
+  }
+ 
+  cancel(): void {
+    this.dialogRef.close(false);
+  }
 }
+
